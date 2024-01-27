b00c84024a94b02a9b5b4a3b06e6977674d674e1
browncracker <nealiumkansara@gmail.com>
2024-01-27 12:11:12 -0500

    basic html layout and math functions done

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNums(a, b) {
+    return a + b;
+}
+
+function subNums(a, b) {
+    return a - b;
+}
+
+function multNums(a, b) {
+    return a * b;
+}
+
+function divNums(a, b) {
+    return a / b;
+}
